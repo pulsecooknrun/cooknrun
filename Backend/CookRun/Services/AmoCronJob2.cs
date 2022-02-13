443cd07485cfe4452f39c81a095b0eafd7919676
Belorad <pertsevy@yandex.ru>
2022-02-13 22:16:11 +0300

    всё обратно

--- Backend/CookRun/Services/AmoCronJob2.cs
+++ Backend/CookRun/Services/AmoCronJob2.cs
@@ -119,7 +119,6 @@ namespace CookRun.Services
                     }
                     else
                     {
-                        amoDayReport.ProjectName = user.Name;//может так префиксы уберутся
                         amoDayReport.Leads = leads;
                         amoDayReport.Closed = closed;
                         amoDayReport.LeadsWithoutTasks = leadsWithoutTasks;
